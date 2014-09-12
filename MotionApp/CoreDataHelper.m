03482889a906d9589719cabe05f7a608faf3ebb5
mhkane <mhkane@mit.edu>
2014-09-12 11:41:58 -0400

    app renamed

---
+++ MotionApp/CoreDataHelper.m
@@ -0,0 +1,13 @@
+//
+//  CoreDataHelper.m
+//  Motion App
+//
+//  Created by Mohamed Kane on 9/12/14.
+//  Copyright (c) 2014 j_siegel. All rights reserved.
+//
+
+#import "CoreDataHelper.h"
+
+@implementation CoreDataHelper
+
+@end
