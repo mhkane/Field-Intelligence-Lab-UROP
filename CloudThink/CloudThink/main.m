716113f933920078ddcc18e9ed82335da46dcb8f
mhkane <mhkane@mit.edu>
2014-09-12 11:17:45 -0400

    Repository for my UROPs
    Contain main projects for Field Intelligence Lab UROP

---
+++ CloudThink/CloudThink/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CloudThink
+//
+//  Created by Mohamed Kane on 9/12/14.
+//  Copyright (c) 2014 Mohamed Kane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
