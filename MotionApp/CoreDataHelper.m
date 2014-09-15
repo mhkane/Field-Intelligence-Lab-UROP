ca52a7e4f4fb1def0785782e6ab0c85afeb6952a
mhkane <mhkane@mit.edu>
2014-09-15 11:46:39 -0400

    First part of core Data Helper classes

--- MotionApp/CoreDataHelper.m
+++ MotionApp/CoreDataHelper.m
@@ -10,4 +10,69 @@
 
 @implementation CoreDataHelper
 
+#define debug 1
+
+#pragma mark - FILES
+//Name for the database where the data is sent
+NSString *storeFilename = @"motionApp.sqlite";
+
+#pragma mark - PATHS
+//Return the application document directory
+- (NSString *)applicationDocumentsDirectory{
+    if(debug ==1){
+        NSLog(@"Running %@ '%@'", self.class, NSStringFromSelector(_cmd));
+    }
+    return [NSSearchPathForDirectoriesInDomains(NSDocumentationDirectory, NSUserDomainMask, YES) lastObject];
+}
+//Appends a directory called Stores to the application's documents directory and then returns it.
+- (NSURL *)applicationStoresDirectory {
+    if (debug==1) {
+        NSLog(@"Running %@ '%@'", self.class, NSStringFromSelector(_cmd));
+    }
+    
+    NSURL *storesDirectory =
+    [[NSURL fileURLWithPath:[self applicationDocumentsDirectory]]
+     URLByAppendingPathComponent:@"Stores"];
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    if (![fileManager fileExistsAtPath:[storesDirectory path]]) {
+        NSError *error = nil;
+        if ([fileManager createDirectoryAtURL:storesDirectory
+                  withIntermediateDirectories:YES
+                                   attributes:nil
+                                        error:&error]) {
+            if (debug==1) {
+                NSLog(@"Successfully created Stores directory");}
+        }
+        else {NSLog(@"FAILED to create Stores directory: %@", error);}
+    }
+    return storesDirectory;
+}
+//Appends the persistent store filename to the stores directory path
+- (NSURL *)storeURL {
+    if (debug==1) {
+        NSLog(@"Running %@ '%@'", self.class, NSStringFromSelector(_cmd));
+    }
+    return [[self applicationStoresDirectory]
+            URLByAppendingPathComponent:storeFilename];
+}
+
+#pragma mark -SETUP
+- (id)init {
+    if (debug==1) {
+        NSLog(@"Running %@ '%@'", self.class, NSStringFromSelector(_cmd));
+    }
+    self = [super init];
+    if (!self) {return nil;}
+    
+    _model = [NSManagedObjectModel mergedModelFromBundles:nil];
+    _coordinator = [[NSPersistentStoreCoordinator alloc]
+                    initWithManagedObjectModel:_model];
+    _context = [[NSManagedObjectContext alloc]
+                initWithConcurrencyType:NSMainQueueConcurrencyType];
+    [_context setPersistentStoreCoordinator:_coordinator];
+    return self;
+}
+
+
 @end
